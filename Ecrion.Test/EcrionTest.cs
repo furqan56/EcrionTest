4f78921cf5298d467e3036042a5654ae85962574
Sumair Javed <sumair.javed@clickchain.com>
2018-05-29 16:04:12 +0500

    Changes for browser integration in test

--- Ecrion.Test/EcrionTest.cs
+++ Ecrion.Test/EcrionTest.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.IO;
-using System.Windows.Forms;
 using BridgeSuite.Ecrion.Service;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NHtmlUnit;
-using NHtmlUnit.Html;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Ecrion.Test
 {
@@ -12,6 +12,7 @@ namespace Ecrion.Test
     public class EcrionTest
     {
         private EcrionService _ecrionService;
+        private IWebDriver driver;
 
         public EcrionTest()
         {
@@ -23,16 +24,56 @@ namespace Ecrion.Test
         {
             if (!Directory.Exists("..\\..\\Files\\Temp"))
                 Directory.CreateDirectory("..\\..\\Files\\Temp");
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("file:///C:/Users/CLICK%20CHAIN/Downloads/bcl_2031189454.htm");
         }
 
         [TestMethod]
         public void TestMethod1()
         {
-            var htmlFilePath =RenderHtml("..\\..\\Files\\Kaiser\\KaiserSureNet_OutReach.xml", "KP:KP_B12_BS_PROD.xfd");
-            var page =new WebClient().GetHtmlPage(htmlFilePath);
+            var htmlFilePath = RenderHtml("..\\..\\Files\\Kaiser\\KaiserSureNet_OutReach.xml", "KP:KP_B12_BS_PROD.xfd");
+            var page = new WebClient().GetHtmlPage(htmlFilePath);
         }
 
-        private string RenderHtml(string xmlPath,string template)
+
+        [TestMethod]
+        public void VerifyHeaderNameInPdf()
+        {
+            var value = driver.FindElement(By.ClassName("td1"));
+            Assert.AreEqual(value.Text, "FORMULARY CHANGE NOTICE");
+        }
+
+        [TestMethod]
+        public void VerifyDateInPdf()
+        {
+            var value = driver.FindElement(By.ClassName("td0"));
+            Assert.AreEqual(value.Text, "December 1, 2017");
+        }
+
+        [TestMethod]
+        public void VerifyFirstNameInPdf()
+        {
+            var value = driver.FindElement(By.ClassName("td0"));
+            Assert.AreEqual(value.Text, "December 1, 2017");
+        }
+
+        [TestMethod]
+        public void VerifyLastNameInPdf()
+        {
+            var value = driver.FindElement(By.ClassName("td0"));
+            Assert.AreEqual(value.Text, "December 1, 2017");
+        }
+
+        [TestMethod]
+        public void VerifyAddress1InPdf()
+        {
+            var value = driver.FindElement(By.ClassName("td0"));
+            Assert.AreEqual(value.Text, "December 1, 2017");
+        }
+
+        private string RenderHtml(string xmlPath, string template)
         {
             var xml = File.ReadAllText(xmlPath);
             var htmlbytes = _ecrionService.Render(xml, template);
@@ -44,7 +85,8 @@ namespace Ecrion.Test
         [TestCleanup]
         public void TearDown()
         {
-          Directory.Delete("..\\..\\Files\\Temp",true);  
+            Directory.Delete("..\\..\\Files\\Temp", true);
+            driver.Close();
         }
     }
 }
