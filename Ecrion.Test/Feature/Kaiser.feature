34376556dd3b1bc3867ff841035eb2e4657e2488
Sumair Javed <sumair.javed@clickchain.com>
2018-05-29 20:26:58 +0500

    Kaiser Test Add

---
+++ Ecrion.Test/Feature/Kaiser.feature
@@ -0,0 +1,10 @@
+﻿Feature: Kaiser
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Kaiser Outreach Template
+	Given the XML for Kaiser Outreach Template
+	When template generated from Ecrion
+	Then PDF should render on Kaiser Outreach Template with XML Data
---
+++ Ecrion.Test/Feature/KaiserSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Ecrion.Test.Feature
+{
+    [Binding]
+    public class KaiserSteps
+    {
+        [Given(@"the XML for Kaiser Outreach Template")]
+        public void GivenTheXmlForKaiserOutreachTemplate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"template generated from Ecrion")]
+        public void WhenTemplateGeneratedFromEcrion()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"PDF should render on Kaiser Outreach Template with XML Data")]
+        public void ThenPdfShouldRenderOnKaiserOutreachTemplateWithXmlData()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
